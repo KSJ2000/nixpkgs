a7fc5fdcb7f0597e7711bc322016d3016166b8be
KSJ2000 <katsho123@outlook.com>
2025-03-22 10:24:18 +0200

    weylus: move to by-name

--- nixos/modules/programs/weylus.nix
+++ nixos/modules/programs/weylus.nix
@@ -11,7 +11,6 @@ in
 {
   options.programs.weylus = with lib.types; {
     enable = lib.mkEnableOption "weylus, which turns your smart phone into a graphic tablet/touch screen for your computer";
-
     openFirewall = lib.mkOption {
       type = bool;
       default = false;
@@ -19,7 +18,6 @@ in
         Open ports needed for the functionality of the program.
       '';
     };
-
     users = lib.mkOption {
       type = listOf str;
       default = [ ];
@@ -28,9 +26,16 @@ in
         These users can synthesize input events system-wide, even when another user is logged in - untrusted users should not be added.
       '';
     };
-
-    package = lib.mkPackageOption pkgs "weylus" { };
+    package = lib.mkOption {
+      type = package;
+      default = pkgs.callPackage ../../../pkgs/by-name/we/weylus/package.nix { };
+      defaultText = lib.literalExpression "pkgs.callPackage ../../../pkgs/by-name/we/weylus/package.nix {}";
+      description = ''
+        The weylus package to use.
+      '';
+    };
   };
+
   config = lib.mkIf cfg.enable {
     networking.firewall = lib.mkIf cfg.openFirewall {
       allowedTCPPorts = [
@@ -38,11 +43,8 @@ in
         9001
       ];
     };
-
     hardware.uinput.enable = true;
-
     users.groups.uinput.members = cfg.users;
-
     environment.systemPackages = [ cfg.package ];
   };
 }
